6ea77f54e4d8907b4dea0895b7f2a53171c05e71
Thayna Santana <thaynamenezes01@gmail.com>
2019-10-21 16:35:50 -0400

    database pre-set working

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//conect mongodb
+var monk = require('monk');
+var db = monk('localhost:27017/api');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,6 +22,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// make my db accessible to my router 
+app.use(function(req, res, next){
+  req.db = db;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,15 @@ router.get('/hello', function(req, res){
   res.render('hello', {title: 'Hello, Stranger!'});
 });
 
+// GET userlist page
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist', {
+      "userlist" : docs
+    });
+  }); 
+});
+
 module.exports = router;
